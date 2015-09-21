455d216cc9144410f0a3d05cd63fbcbbf8e4e0c7
Brian Hedberg <bshslowfood@gmail.com>
2015-09-21 18:14:44 -0500

    add sass-rails

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,8 @@
 require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.sass.preferred_syntax = :sass
+  config.sass.line_comments = false
+  config.sass.cache = false
+end
