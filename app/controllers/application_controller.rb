d0e6a6a6834d1fed0c607e6bc3dd23c632bd227c
Jessie A. Young <jessica.young@gsa.gov>
2015-09-23 10:59:42 -0700

    Add basic auth as stopgap
    * Fixes #25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      end
+    end
+  end
 end
