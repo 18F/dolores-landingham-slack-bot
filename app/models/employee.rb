41c27d806410785f94e67a51859007ac9cd1eab4
Jessie A Young <jessica.young@gsa.gov>
2016-05-17 15:23:14 -0700

    Validate uniqueness of Employee slack username with scope
    * We are using paranoia to soft delete
    * Need to scope uniqueness validations to records that are not deleted

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,10 +3,16 @@ class Employee < ActiveRecord::Base
 
   has_many :sent_scheduled_messages, dependent: :destroy
 
-  validates :slack_username,
-    presence: true,
-    uniqueness: true,
-    format: { with: /\A[a-z_0-9.]+\z/, message: "Slack usernames can only contain lowercase letters, numbers, underscores, and periods." }
+  validates :slack_username, presence: true
+  validates_uniqueness_of :slack_username, conditions: -> { where(deleted_at: nil) }
+  validates(
+    :slack_username,
+    format: {
+      with: /\A[a-z_0-9.]+\z/,
+      message: "Slack usernames can only contain lowercase letters, numbers, underscores, and periods."
+    }
+  )
+
   validates :started_on, presence: true
   validates :time_zone, presence: true
 
