4f2dc1799ce740bd4efaf0e706210db22c35ef64
Jessie A. Young <jessie.a.young@gmail.com>
2016-07-01 15:55:11 -0700

    Remove unused SMTP_SETTINGS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
