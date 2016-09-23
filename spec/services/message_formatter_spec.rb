4dcdc6b63572678f8bfe4b1c85283a8219a3a8d1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-09-23 15:43:45 -0500

    Use correct factory in the MessageFormatter spec
    This commit switches the `scheduled_message` factory (which no longer exists) with the `onboarding_message` factory in the spec for the MessageFormatter.

--- spec/services/message_formatter_spec.rb
+++ spec/services/message_formatter_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe MessageFormatter do
   describe "#escape_slack_characters" do
     it "escapes specific characters (&, <, and >) as defined by the Slack Message API" do
-      scheduled_message = create(
-        :scheduled_message,
+      message = create(
+        :onboarding_message,
         body: "This is a \"message\" with &, <, and > characters that should be escaped and some 'others' that _should_ *not* be!"
       )
-      formatted_message_body = MessageFormatter.new(scheduled_message).escape_slack_characters
+      formatted_message_body = MessageFormatter.new(message).escape_slack_characters
 
       expect(formatted_message_body).to eq(
           "This is a \"message\" with &amp;, &lt;, and &gt; characters that should be escaped and some 'others' that _should_ *not* be!",
