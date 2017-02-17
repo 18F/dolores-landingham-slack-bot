98399c94a7d15691ac2b57f9a565349d9eaf8e2a
James C. Scott <jamesc.scott@gsa.gov>
2017-02-17 10:31:07 -0500

    Update deployer credentials for each space
    Also, update cli and flags to only use one hyphen

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,23 @@
 set -e
 
-API="https://api.cloud.gov"
-ORG="18f"
-SPACE="dolores"
-ENVIRONMENT=$1
+API="https://api.fr.cloud.gov"
+ORG="gsa-18f-dolores"
+SPACE=$1
 
-if [ $ENVIRONMENT = 'production' ]; then
+if [ $SPACE = 'prod' ]; then
   NAME="dolores-app"
-elif [ $ENVIRONMENT = 'staging' ]; then
+  CF_USERNAME=$CF_USERNAME_PRODUCTION
+  CF_PASSWORD=$CF_PASSWORD_PRODUCTION
+  MANIFEST="manifest.yml"
+elif [ $SPACE = 'staging' ]; then
   NAME="dolores-staging"
+  CF_USERNAME=$CF_USERNAME_STAGING
+  CF_PASSWORD=$CF_PASSWORD_STAGING
+  MANIFEST="manifest_staging.yml"
 else
-  echo "Unknown environment: $ENVIRONMENT"
+  echo "Unknown space: $SPACE"
   exit
 fi
 
-cf login --a $API --u $CF_USERNAME --p $CF_PASSWORD --o $ORG -s $SPACE
-cf push $NAME
+cf login -a $API -u $CF_USERNAME -p $CF_PASSWORD -o $ORG -s $SPACE
+cf push -f $MANIFEST
