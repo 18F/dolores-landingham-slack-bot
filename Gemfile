4ec4f150405f7689360ab93f03202c4a6e98fd78
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-11-20 10:34:51 -0600

    Update ruby version to 2.3.5

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "2.3.3"
+ruby "2.3.5"
 gem "rails", "~> 5.0.0"
 
 gem "active_record_union"
--- db/schema.rb
+++ db/schema.rb
@@ -88,10 +88,10 @@ ActiveRecord::Schema.define(version: 20170310195553) do
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
-    t.integer  "taggable_id"
     t.string   "taggable_type"
-    t.integer  "tagger_id"
+    t.integer  "taggable_id"
     t.string   "tagger_type"
+    t.integer  "tagger_id"
     t.string   "context",       limit: 128
     t.datetime "created_at"
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
